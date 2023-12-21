e1758d879bd74176198e35f103f82f41bb8a6ddd
nahuelprinsich <nahuelprinsich@outlook.com>
2023-12-21 16:33:53 -0300

    added refreshControl to event list

--- src/screens/eventList/index.tsx
+++ src/screens/eventList/index.tsx
@@ -1,17 +1,20 @@
-import { FlatList, View } from "react-native";
+import { useState } from "react";
+import { FlatList, RefreshControl, View } from "react-native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 
 import { useGetEventsQuery } from '../../services/apis/event';
 import styles from './styles';
 import { ApplicationNavigatorParamList } from "../../navigators/Application";
 import { Header, ListItem } from "../../components";
+
 type Props = {
     navigation: NativeStackNavigationProp<ApplicationNavigatorParamList, 'EventList' >;  
 };
 
 const EventList: React.FC<Props> = ({ navigation }) => {
 
-    const { data } = useGetEventsQuery();
+    const [refreshing, setRefreshing] = useState(false);
+    const { data, refetch } = useGetEventsQuery();
 
     const goToDetail = (item: any) => {
         navigation.navigate('EventDetail', { item })
@@ -28,6 +31,7 @@ const EventList: React.FC<Props> = ({ navigation }) => {
                         renderItem={({item}) => <ListItem item={item} onPress={() => goToDetail(item)}/>}
                         keyExtractor={item => item.id}
                         ListHeaderComponent={<Header title="Art Institute of Chicago"/>}
+                        refreshControl={<RefreshControl refreshing={refreshing} onRefresh={refetch} />}
                     />
                 }
             </View>
