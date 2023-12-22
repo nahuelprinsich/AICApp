0579a1b3f875ed5c5ffcf6579dcef0a83329adaa
nahuelprinsich <nahuelprinsich@outlook.com>
2023-12-22 12:17:03 -0300

    added animation to event and favorites list

--- src/components/listItem/index.tsx
+++ src/components/listItem/index.tsx
@@ -1,26 +1,51 @@
-import { Image, Text, TouchableOpacity, View } from "react-native";
+import { useRef } from "react";
+import { Image, Text, TouchableOpacity, View, Animated, Easing } from "react-native";
 
 import styles from './styles';
 
 const ListItem = ({ item, onPress }) => {
 
+    const translateY = useRef(new Animated.Value(100)).current;
+
+    Animated.timing(translateY, {
+        toValue: 0,
+        duration: 500,
+        useNativeDriver: true,
+        easing: Easing.out(Easing.cubic),
+    }).start();
+
+    const slideInAnimation = {
+        transform: [
+            {
+                translateY: translateY.interpolate({
+                    inputRange: [0, 100],
+                    outputRange: [0, 100],
+                }),
+            },
+        ],
+    };
+
     return (
-        <TouchableOpacity style={styles.card}
-            onPress={() => onPress()}>
-            <View style={styles.imageContainer}>
-                <Image
-                    style={styles.imageStyle}
-                    source={{
-                        uri: item.image_url,
-                    }}
-                />
-            </View>
-            <View style={styles.textContainer}>
-                <Text>
-                    { item.title }
-                </Text>
-            </View>
-        </TouchableOpacity>
+        <Animated.View style={[slideInAnimation]}>
+            <TouchableOpacity 
+                style={styles.card}
+                onPress={() => onPress()}
+            >
+                <View style={styles.imageContainer}>
+                    <Image
+                        style={styles.imageStyle}
+                        source={{
+                            uri: item.image_url,
+                        }}
+                    />
+                </View>
+                <View style={styles.textContainer}>
+                    <Text>
+                        { item.title }
+                    </Text>
+                </View>
+            </TouchableOpacity>
+        </Animated.View>
     );
     
 };
